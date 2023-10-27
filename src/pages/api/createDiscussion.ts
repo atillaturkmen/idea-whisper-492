4a969d7e7aaf00ea295ebbdb54b68c826e75908e
atillaturkmen <turkmenatilla522@gmail.com>
2023-10-27 15:46:11 +0300

    added mysql connection with prisma

--- src/pages/api/createDiscussion.ts
+++ src/pages/api/createDiscussion.ts
@@ -1,12 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from "@/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { idea, startDate, endDate, allowVoteVisibility, allowMultipleSelections, maxSelections } = req.body;
 
-        // Here, you'd store the data in your database.
-        // For demonstration, let's pretend we've done that and return a success response.
+        const newDiscussionPost = await prisma.discussionPost.create({
+            data: {
+                topic: idea, // You can set this value based on your requirements
+                vote_start_date: startDate,
+                vote_end_date: endDate,
+                can_see_votes_during_voting: allowVoteVisibility,
+                max_nof_selections: maxSelections,
+                will_be_voted: allowMultipleSelections,
+                admin_link: "https://example.com",
+            }
+        });
+
         console.log(idea, startDate, endDate, allowVoteVisibility, allowMultipleSelections, maxSelections);
+        console.log(newDiscussionPost);
 
         return res.status(200).json({ success: true });
     }
