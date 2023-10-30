538f7fe3256de4aa84652d82a678583a70290d21
atillaturkmen <turkmenatilla522@gmail.com>
2023-10-30 18:18:17 +0300

    create admin and visitor links

--- src/pages/api/createDiscussion.ts
+++ src/pages/api/createDiscussion.ts
@@ -5,6 +5,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         const { idea, startDate, endDate, allowVoteVisibility, allowMultipleSelections, maxSelections } = req.body;
 
+        const randomAdminLink = generateRandomLink(255);
+
+        const randomVisitorLink = generateRandomLink(255);
+
         const newDiscussionPost = await prisma.discussionPost.create({
             data: {
                 topic: idea, // You can set this value based on your requirements
@@ -13,7 +17,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 can_see_votes_during_voting: allowVoteVisibility,
                 max_nof_selections: maxSelections,
                 will_be_voted: allowMultipleSelections,
-                admin_link: "https://example.com",
+                admin_link: randomAdminLink,
+                VisitorLink: {
+                    create: {
+                        link: randomVisitorLink,
+                    },
+                },
             }
         });
 
@@ -23,4 +32,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(200).json({ success: true });
     }
     return res.status(405).end(); // Method Not Allowed
+}
+
+function generateRandomLink(length: number): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let link = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        link += characters.charAt(randomIndex);
+    }
+
+    return link;
 }
