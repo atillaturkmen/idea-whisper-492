24690b609ae9362887f9e9ef606751bdbd7fe3dc
Onur Kömürcü <onurkomurcu@Onurs-MBP.home>
2023-11-05 02:08:52 +0300

    Receive ideas endpoint is implemented.

--- src/pages/api/receiveDiscussion.ts
+++ src/pages/api/receiveDiscussion.ts
@@ -4,14 +4,38 @@ import prisma from "@/prisma";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         const { link } = req.query;
-        console.log(link);
-        const data = await prisma.discussionPost.findFirst({
+        let discussion = await prisma.discussionPost.findFirst({
             where: {
                 admin_link: String(link)
             }
         });
 
-        return res.status(200).json(data);
+        if (!discussion) {
+            let visitor_link = await prisma.visitorLink.findFirst({
+                where: {
+                    link: String(link) 
+                }
+            });
+            if (!visitor_link) {
+                return res.status(404).json(discussion);
+            }
+            discussion = await prisma.discussionPost.findFirst({
+                where: {
+                    id: visitor_link.idDiscussionPost
+                }
+            });
+            const newDiscussion = {
+                ...discussion,
+                is_admin: false,
+            };
+            return res.status(200).json(newDiscussion);
+        }
+        const newDiscussion = {
+            ...discussion,
+            is_admin: true,
+        };
+        
+        return res.status(200).json(newDiscussion);
     }
-    return res.status(405).end(); // Method Not Allowed
+    return res.status(405).end(); 
 }
--- src/pages/discussion-page.tsx
+++ src/pages/discussion-page.tsx
@@ -1,57 +1,89 @@
 import {useRouter} from "next/router";
 import { resolve } from "path";
-import React from "react";
+import React, { use } from "react";
 import {useState} from "react";
 import {useEffect} from "react";
-import Router from "next/router";
-
+import styles_page from '../styles/DiscussionPage.module.css';
 
 const DiscussionPage: React.FC = () => {
-    const [discussion, setDiscussion] = useState("");
+    const [discussionID, setDiscussionID] = useState(Number);
+    const [topic, setTopic] = useState("");
     const [ideas, setIdeas] = useState<string[]>([]);
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [endDate, setEndDate] = useState<Date | null>(null);
+    const [remainingDaysToVote, setRemainingDaysToVote] = useState(0);
     const [allowVoteVisibility, setAllowVoteVisibility] = useState(false);
     const [allowMultipleSelections, setAllowMultipleSelections] = useState(false);
+    const [admin, setAdmin] = useState(false);
     const [maxSelections, setMaxSelections] = useState(1);
     const router = useRouter();
 
-
-    const handleReceiveDiscussion = async (admin_link: string) => {
-        const url = `/api/receiveDiscussion?link=${admin_link}`;
-        
+    const handleReceiveDiscussion = async (link: string) => {
+        const url = `/api/receiveDiscussion?link=${link}`;
         const response = await fetch(url, {
             method: 'GET',
             headers: {'Content-Type': 'application/json'}
         });
-        try {
-            const data = await response.json();
-            if (data.status == 200) {
-                setDiscussion(data.topic);
-                //setIdeas(data.ideas);
-                //setStartDate(data.vote_start_date);
-                //setEndDate(data.vote_end_date);
-                setAllowVoteVisibility(data.can_see_votes_during_voting);
-                setAllowMultipleSelections(data.will_be_voted);
-                setMaxSelections(data.max_nof_selections);
-            } else {
-                // Directs to index page
-            }
-        } catch (error) {
-            console.log(error);
-        };
-    } 
+
+        const discussion = await response.json();
+        if (discussion) {
+            setTopic(discussion.topic);
+            setDiscussionID(discussion.id);
+            setStartDate(discussion.vote_start_date);
+            setEndDate(discussion.vote_end_date);
+            setAllowVoteVisibility(discussion.can_see_votes_during_voting);
+            setAllowMultipleSelections(discussion.will_be_voted);
+            setMaxSelections(discussion.max_nof_selections);
+            setAdmin(discussion.is_admin);
+        } else { router.push("/"); }
+    }
+
+    const handleReceiveIdeas = async (discussion_id: number) => {
+        const url = `/api/receiveIdeas?discussion_id=${discussion_id}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        });
+        const ideas = await response.json();
+        if (ideas) {
+            setIdeas(ideas);
+            console.log(ideas);
+        }
+    }
+
     useEffect(() => {
-        const admin_link = router.query.link;
-        if (admin_link) {
-            handleReceiveDiscussion(admin_link as string);
+        const link = router.query.link;
+        if (link) {
+            handleReceiveDiscussion(link as string);
+            handleReceiveIdeas(discussionID as number);
         }
     }, [router.query]);
+
+    useEffect(() => {
+        handleReceiveIdeas(discussionID as number);
+    }, [discussionID]);
+
+    useEffect(() => {
+        if (endDate !== null && endDate !== undefined) {
+            const now = new Date();
+            const remainingTime = Date.parse(endDate.toString())- now.getTime();
+            const remainingDays = Math.ceil(remainingTime / (1000 * 60 * 60 * 24));
+            setRemainingDaysToVote(remainingDays);
+        }
+      }, [endDate]);
     
     return (
-        <div>
-            <h1>Idea Whisper</h1>
-            <h2>{discussion}</h2>
+        <div className={styles_page.container}>
+            {startDate !== null && endDate !== null ? (
+                    <div className={styles_page.timer}> {remainingDaysToVote} days left to vote</div>
+            ) : null}
+            <div className={styles_page.box}>
+                <div className={styles_page.topic}>
+                    <h1>{topic}</h1>
+                </div>
+            </div>
+            <h3>{discussionID}</h3>
+            <h4>{allowVoteVisibility.toString()}</h4>
         </div>
     );
 }
