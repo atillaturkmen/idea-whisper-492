d4a754a5b8e2152205f24feb5cd1849935ebf150
atillaturkmen <turkmenatilla522@gmail.com>
2023-12-09 12:37:16 +0300

    deleted unnecessary imports

--- src/pages/discussion-page.tsx
+++ src/pages/discussion-page.tsx
@@ -5,12 +5,11 @@ import {useEffect} from "react";
 import {BsTrash} from 'react-icons/bs';
 import styles from '../styles/DiscussionPage.module.css';
 import {getUserData, writeUserData} from "@/local-storage/userUtils";
-import {max, set} from "date-fns";
 import buttonStyles from '../styles/Button.module.css';
 import Chart from 'chart.js/auto';
 import {useRef} from 'react';
 
-import {ToastContainer, toast} from 'react-toastify';
+import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface VoteChartProps {
